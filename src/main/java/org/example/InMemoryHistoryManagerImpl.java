17acd2ee05d5ff338469a0ffa412e2213238a9f3
Скобелкин Алексей <skobelkin.97@inbox.ru>
2024-12-05 20:02:06 +0300

    Исправил домашнее задание

--- src/main/java/org/example/InMemoryHistoryManagerImpl.java
+++ src/main/java/org/example/InMemoryHistoryManagerImpl.java
@@ -9,94 +9,98 @@ import java.util.*;
 @Setter
 
 public class InMemoryHistoryManagerImpl implements HistoryManager {
-    private final CustomLinkedList customLinkedList = new CustomLinkedList();
 
     @Override
     public List<Task> getHistory() {
-        return customLinkedList.getTasks();
+        return getTasks();
     }
 
     @Override
     public void add(Task task) {
-        customLinkedList.linkLast(task);
+        if (table.containsKey(task.getId())) {
+            remove(task.getId());
+        }
+        linkLast(task);
     }
 
     @Override
     public void remove(int id) {
-        customLinkedList.removeNode(customLinkedList.getNode(id));
+        Node removedNode = table.remove(id);
+
+        if (removedNode != null) {
+            this.removeNode(removedNode);
+        }
     }
 
-    private static class CustomLinkedList {
-        private final Map<Integer, Node> table = new HashMap<>();
-        private Node head;
-        private Node tail;
+    private final Map<Integer, Node> table = new HashMap<>();
+    private Node head;
+    private Node tail;
 
-        private void linkLast(Task task) {
-            Node node = new Node();
-            node.setTask(task);
+    private void linkLast(Task task) {
+        Node node = new Node();
+        node.setTask(task);
 
 
-            if (table.containsKey(task.getId())) {
-                removeNode(table.get(task.getId()));
-            }
-
-            if (head == null) {
-                head = node;
-                tail = node;
-
-                node.setPrev(null);
-                node.setNext(null);
-            } else {
-                node.setPrev(tail);
-                node.setNext(null);
-                tail.setNext(node);
-                tail = node;
-            }
-
-            table.put(task.getId(), node);
-
+        if (table.containsKey(task.getId())) {
+            removeNode(table.get(task.getId()));
         }
 
-        private Node getNode(int id) {
-            return table.get(id);
+        if (head == null) {
+            head = node;
+            tail = node;
+
+            node.setPrev(null);
+            node.setNext(null);
+        } else {
+            node.setPrev(tail);
+            node.setNext(null);
+            tail.setNext(node);
+            tail = node;
         }
 
-        private List<Task> getTasks() {
-            List<Task> list = new ArrayList<>();
-            Node element = head;
+        table.put(task.getId(), node);
 
-            while (element != null) {
-                list.add(element.getTask());
-                element = element.getNext();
+    }
+
+    private Node getNode(int id) {
+        return table.get(id);
+    }
+
+    private List<Task> getTasks() {
+        List<Task> list = new ArrayList<>();
+        Node element = head;
+
+        while (element != null) {
+            list.add(element.getTask());
+            element = element.getNext();
+        }
+
+        return list;
+    }
+
+    private void removeNode(Node node) {
+        if (node != null) {
+            table.remove(node.getTask().getId());
+
+            Node nodeNext = node.getNext();
+            Node nodePrev = node.getPrev();
+
+            if (head == node) {
+                head = node.getNext();
             }
 
-            return list;
-        }
-
-        private void removeNode(Node node) {
-            if (node != null) {
-                table.remove(node.getTask().getId());
-
-                Node nodeNext = node.getNext();
-                Node nodePrev = node.getPrev();
-
-                if (head == node) {
-                    head = node.getNext();
-                }
-
-                if (tail == node) {
-                    tail = node.getPrev();
-                }
-
-                if (nodeNext != null) {
-                    nodeNext.setPrev(nodePrev);
-                }
-                if (nodePrev != null) {
-                    nodePrev.setNext(nodeNext);
-                }
+            if (tail == node) {
+                tail = node.getPrev();
             }
 
+            if (nodeNext != null) {
+                nodeNext.setPrev(nodePrev);
+            }
+            if (nodePrev != null) {
+                nodePrev.setNext(nodeNext);
+            }
         }
+
     }
 
 
